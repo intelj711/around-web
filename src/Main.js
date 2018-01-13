15d849adebddbed24f175b4fb185d1d0e6bbd1a7
FayeZ <im5dii@gmail.com>
2018-01-14 00:02:45 +0800

    add register UI

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import { Register } from './Register';
+import { Register } from './Register';
 
 export class Main extends React.Component {
     render() {
         return (
             <section className="main">
-
+                <Register/>
             </section>
         );
     }
