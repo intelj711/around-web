036c090aacf543be2b6d6242b9b893cdab763486
FayeZ <im5dii@gmail.com>
2018-01-16 14:07:26 +0800

    add local storage

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import { Header } from './Header';
 import { Main } from './Main';
 import '../styles/App.css';
+import {TOKEN_KEY} from '../constants';
 
 class App extends Component {
+    state = {
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY),
+    }
+
+
+    loginHandler = (token) => {
+        this.setState({ isLoggedIn: true });
+        localStorage.setItem(TOKEN_KEY, token);
+    }
+
     render() {
         return (
             <div className="App">
                 <Header/>
-                <Main/>
+                <Main isLoggedIn={this.state.isLoggedIn} loginHandler = {this.loginHandler}/>
             </div>
         );
     }
