49be7cbd1419d0471fad9946557aced4316453b6
FayeZ <im5dii@gmail.com>
2018-01-16 12:07:41 +0800

    add routng

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Register } from './Register';
+import { Login } from './Login';
+import {Switch, Route} from 'react-router-dom';
 
 export class Main extends React.Component {
     render() {
         return (
-            <section className="main">
-                <Register/>
-            </section>
+            <div className="main">
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/:login" component={Login}/>
+                    <Route component={Login}/>
+                </Switch>
+            </div>
         );
     }
 }
